5dd59591924c8dd463b0a8720d014e5df9fd2838
brokencolor <huajianzhiyue@sina.com>
2019-05-04 13:23:14 +0800

    git中多个项目

--- src/main/java/com/mybatis/config/DruidMonitorConfigurer.java
+++ src/main/java/com/mybatis/config/DruidMonitorConfigurer.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 
